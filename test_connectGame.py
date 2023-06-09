1dabfacea19b1531dcce734f12c25e73c63b326a
intri <intriqet@gmail.com>
2023-06-09 16:04:32 -0700

    tryint to fix branch

--- test_connectGame.py
+++ test_connectGame.py
@@ -1,15 +1,24 @@
 import pytest
 from connectGame import *
 
-@pytest.fixture
+
+def test_func_fast():
+    pass
+
+
+@pytest.mark.smoke
 def test_game_defaults():
-	ame = connectGame.gameObject(height=5,length=8)
-	assert game.inProgress() == False
+    ame = connectGame.gameObject(height=5, length=8)
+    assert game.inProgress() == False
 
+
+@pytest.mark.smoke
 def test_game_height_set():
-	ame = connectGame.gameObject(height=5,length=8)
-	assert game.height = 5
+    ame = connectGame.gameObject(height=5, length=8)
+    assert game.height == 5
 
+
+@pytest.mark.smoke
 def test_game_length_set():
-	ame = connectGame.gameObject(height=5,length=8)
-	assert game.length = 8
+    ame = connectGame.gameObject(height=5, length=8)
+    assert game.length == 8
