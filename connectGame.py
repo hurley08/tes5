4925cc1f22665fea4f3c3f91a680704049cecb09
Ron Kristoffer Sy <rsy@Rons-MacBook-Air.local>
2023-06-28 21:16:35 -0700

    fixed typos

--- connectGame.py
+++ connectGame.py
@@ -650,11 +650,11 @@ def main():
                     else:
 
                         print("there are no spaces available")
-                        game.gamesdsdasdsadebug:
+                        if game.debug:
                             print(game.printLineBreak())
         print(game.isWinner)
         tally.append([game.isWinner[1], game.isWinner[2],
-                      game.iasdasdasddsWinner[3], game.playerScore])
+                      game.isWinner[3], game.playerScore])
 
         # game.whose_turn()
     results = game.process_tally(tally)
@@ -667,3 +667,4 @@ def main():
 if __name__ == '__main__':
     cProfile.run('main()')
     cov.stop()
+    cov.save()
